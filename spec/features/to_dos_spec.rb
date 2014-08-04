3369143a163a286181af3bfe344ea2d154621a71
Luke McComb <lucasmccomb@gmail.com>
2014-08-04 10:46:49 -0600

    code & test written, test passing for last story ✅🌟

--- spec/features/to_dos_spec.rb
+++ spec/features/to_dos_spec.rb
@@ -103,4 +103,58 @@ feature "ToDos" do
     end
 
   end
+
+  scenario "User can only see todos they created" do
+    visit "/"
+
+    click_link "Register"
+
+    fill_in "Username", with: "hunta"
+    fill_in "Password", with: "pazzword"
+
+    click_button "Register"
+
+    fill_in "Username", with: "hunta"
+    fill_in "Password", with: "pazzword"
+
+    click_button "Sign In"
+
+    expect(page).to have_content "Welcome, hunta"
+
+    fill_in "What do you need to do?", with: "Get a haircut"
+    click_button "Add ToDo"
+
+    expect(page).to have_content "ToDo added"
+
+    within ".todos" do
+      expect(page).to have_content "Get a haircut"
+    end
+
+    click_button "Sign Out"
+
+    click_link "Register"
+
+    fill_in "Username", with: "luke"
+    fill_in "Password", with: "luke"
+
+    click_button "Register"
+
+    fill_in "Username", with: "luke"
+    fill_in "Password", with: "luke"
+
+    click_button "Sign In"
+
+    expect(page).to have_content "Welcome, luke"
+
+    fill_in "What do you need to do?", with: "Laundry"
+    click_button "Add ToDo"
+
+    expect(page).to have_content "ToDo added"
+
+    within ".todos" do
+      expect(page).to_not have_content "Get a haircut"
+      expect(page).to have_content "Laundry"
+    end
+
+  end
 end
